3f11527e1d13001652909e4eff573df155085d57
David Lev <davidlev050414@gmail.com>
2022-11-10 00:06:28 +0200

    Upgrade to 0.1.3

--- aapyt/__init__.py
+++ aapyt/__init__.py
@@ -4,4 +4,4 @@ from aapyt.aapyt import get_apk_info, Abi, InstallLocation
 __copyright__ = f'Copyright {datetime.now().year} david-lev'
 __license__ = 'MIT'
 __title__ = 'aapyt'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
